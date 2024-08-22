eea578d56d56c12794d7471b6d8851f7cd818616
perezpablo80 <perez.pablo@gmail.com>
2024-08-22 12:00:16 -0300

    Demo show

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# todo_frontend
-Frontend for deploying a demo of frontend with react and vite
+
+# React + Vite + Bootstrap as frontend
+React has been dealing with some issues regarding the instalation so i used Vite as the handler.
+
+For installing it you need: `npm create vite@latest` if you want to create a new one.
+
+Then you get into the folder and run `npm install` to install all the packages.
+
+If you will just use mine, then just run an `npm install` and all the packages will run ok.
+
+For execution just type `npm run dev`
+
+You will need to update the VITE_BACKEND_URL so that is the right one.
---
+++ src/components/Task.jsx
@@ -0,0 +1,114 @@
+import { useForm } from "react-hook-form";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import axios from "axios";
+import { useEffect, useState } from "react";
+//Actions: add/modify/delete
+function Task({ action = "add", task = false, setMessage = false }) {
+	const { register, handleSubmit, watch, setValue } = useForm();
+	const [title, setTitle] = useState("");
+	function onSubmit(data) {
+		if (action === "add") {
+			axios
+				.post(import.meta.env.VITE_BACKEND_URL + "/todo", { data: data })
+				.then(function (response) {
+					setMessage("Task added, System will  refresh.");
+				})
+				.catch((e) => {
+					console.log("error adding task:");
+					console.log(e);
+				});
+		} else {
+			if (action === "edit") {
+				axios
+					.put(import.meta.env.VITE_BACKEND_URL + "/todo", { data: data })
+					.then(function (response) {
+						setMessage("Task edition completed, System will refresh.");
+						console.log(response);
+					})
+					.catch((e) => {
+						console.log("error editing task:");
+						console.log(e);
+					});
+			} else {
+				alert("No action selected, please verify and try again");
+			}
+		}
+	}
+	if (action === "delete") {
+		if (task) {
+			action = "";
+			axios
+				.delete(import.meta.env.VITE_BACKEND_URL + "/todo", { data: task })
+				.then(function (response) {
+					setMessage("Delete of task done. System will refresh");
+				})
+				.catch((e) => {
+					console.log("Error deleting:", e);
+					setMessage("Delete of task with error. System will  refresh");
+				});
+		} else {
+			alert("You need to select a Task in order to delete it");
+		}
+	}
+	useEffect(() => {
+		if (action === "edit" || action === "delete") {
+			if (task) {
+				setValue("task", task.task);
+				setValue("completed", task.completed);
+				setValue("id", task.id);
+				if (action === "edit") {
+					setTitle("Edit Task");
+				} else {
+					if (action === "delete") {
+						setTitle("Delete Task");
+					}
+				}
+			}
+		}
+		if (action === "add") {
+			setTitle("Add new task");
+		}
+	}, [task, action]);
+
+	return (
+		<Form onSubmit={handleSubmit(onSubmit)}>
+			<h3>{title}</h3>
+			{action !== "add" && (
+				<Form.Group className="mb-3" controlId="Form.TextArea1" hidden={true}>
+					<Form.Label>Id</Form.Label>
+					<Form.Control as="input" {...register("id")} />
+				</Form.Group>
+			)}
+			<Form.Group className="mb-3" controlId="Form.TextArea1">
+				<Form.Label>Task</Form.Label>
+				<Form.Control as="textarea" rows={3} {...register("task")} />
+			</Form.Group>
+			<Form.Group className="mb-3" controlId="Form.ControlInput1">
+				<Form.Check // prettier-ignore
+					type="switch"
+					id="custom-switch"
+					label="Completed"
+					{...register("completed")}
+				/>
+			</Form.Group>
+			<Form.Group>
+				<Button variant="success" type="submit">
+					Submit
+				</Button>
+			</Form.Group>
+		</Form>
+	);
+
+	// return (
+	// 	<form onSubmit={handleSubmit(onSubmit)}>
+	// 		<div>
+	// 			<label htmlFor="usernameInput">Username:</label>
+	// 			<input id="usernameInput" name="inputname" type="text" {...register("inputName")} />
+	// 			<input id="usernameInput2" name="inputname2" type="text" {...register("inputName2")} />
+	// 		</div>
+	// 		<button type="submit">Submit</button>
+	// 	</form>
+	// );
+}
+export default Task;
